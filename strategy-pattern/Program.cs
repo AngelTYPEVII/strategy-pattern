629c1d5de6ce6b478267ab9d8f621c96d9d251f2
Tesl1k <danildavydov161@mail.ru>
2022-01-28 12:46:50 +0300

    LR01

--- strategy-pattern/Program.cs
+++ strategy-pattern/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DUCKLibrary;
 
 namespace strategy_pattern
 {
@@ -10,6 +11,19 @@ namespace strategy_pattern
     {
         static void Main(string[] args)
         {
+            MallardDuck mallardDuck = new MallardDuck();
+            RedheadDuck redheadDuck = new RedheadDuck();
+
+            Duck[] ducks = new Duck[] { mallardDuck, redheadDuck };
+
+            for (int i = 0; i < ducks.Length; i++)
+            {
+                Console.WriteLine(ducks[i].Quack());
+                Console.WriteLine(ducks[i].Swim());
+                Console.WriteLine(ducks[i].Display());
+            }
+
+            Console.ReadKey();
         }
     }
 }
